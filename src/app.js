6a2c3c2ccfc226aa2f29b7c229832c9094d82d99
Pancho Reyes <freyes7194@utm.edu.ec>
2026-08-07 17:46:00 -0500

    Actualizar CORS frontend Render

--- src/app.js
+++ src/app.js
@@ -1,94 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-
-require("dotenv").config();
-
-
-const conectarDB = require("./config/database");
-
-
-const ticketRoutes = require("./routes/ticketRoutes");
-
-
-// Crear aplicación Express
-
-const app = express();
-
-
-// Conectar MongoDB Atlas
-
-conectarDB();
-
-
-
-// ===============================
-// MIDDLEWARES
-// ===============================
-
-
-// Permitir conexión con React
-
 app.use(
     cors({
-
-        origin:[
-            process.env.FRONTEND_URL,
+        origin: [
+            "https://helpdesk-frontend-react.onrender.com",
             "http://localhost:5173"
         ],
-
         methods:[
             "GET",
             "POST",
             "PUT",
             "DELETE"
         ],
-
         credentials:true
-
     })
 );
-
-
-
-// Permitir JSON
-
-app.use(
-    express.json()
-);
-
-
-
-
-// ===============================
-// RUTA DE PRUEBA
-// ===============================
-
-app.get("/",(req,res)=>{
-
-
-    res.json({
-
-        mensaje:
-        "API Help Desk funcionando correctamente"
-
-    });
-
-
-});
-
-
-
-
-// ===============================
-// RUTAS DEL SISTEMA
-// ===============================
-
-
-app.use(
-    "/tickets",
-    ticketRoutes
-);
-
-
-
-module.exports = app;
